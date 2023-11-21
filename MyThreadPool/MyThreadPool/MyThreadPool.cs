5a998e862d2d3a9afde027c71f579e9abac4c1bf
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-11-21 04:15:01 +0300

    implement threadpool threads

--- MyThreadPool/MyThreadPool/MyThreadPool.cs
+++ MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -1,6 +1,58 @@
-﻿namespace MyThreadPool;
+﻿using System.Collections.Concurrent;
+using System.Threading;
 
-public class Class1
+namespace MyThreadPool;
+
+public class MyThreadPool
 {
+    private ConcurrentQueue<Action> tasksQueue = new();
+    private AutoResetEvent queueIsNotEmptyEvent = new(false);
+    private ManualResetEvent shutDownEvent = new(false);
+    private WaitHandle[] events;
+    private Thread[] threads;
+    private CancellationTokenSource source = new();
+    private volatile int workingThreadsNumber = 0;
 
+    public MyThreadPool(int threadsNumber)
+    {
+        events = new WaitHandle[]
+        {
+            queueIsNotEmptyEvent,
+            shutDownEvent
+        };
+        threads = new Thread[threadsNumber];
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i] = new Thread(() =>
+            {
+                while (!source.IsCancellationRequested)
+                {
+                    int eventIndex = WaitHandle.WaitAny(events);
+                    if (eventIndex == 1)
+                        break;
+                    queueIsNotEmptyEvent.Reset()
+                    if (!tasksQueue.TryDequeue(out Action? task))
+                        continue;
+                    else if (task != null)
+                    {
+                        workingThreadsNumber++;
+                        task();
+                        workingThreadsNumber--;
+                    }
+                }
+                while (tasksQueue.Count > 0)
+                {
+                    if (!tasksQueue.TryDequeue(out Action? task))
+                        break;
+                    else if (task != null)
+                    {
+                        workingThreadsNumber++;
+                        task();
+                        workingThreadsNumber--;
+                    }
+                }
+            });
+            threads[i].Start();
+        }
+    }
 }
