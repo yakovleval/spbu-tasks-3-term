9c8f8e09c63fe1c67f87f4f95beef93168e88a85
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-11-21 20:36:54 +0300

    implement IMyTask interface

--- MyThreadPool/MyThreadPool/IMyTask.cs
+++ MyThreadPool/MyThreadPool/IMyTask.cs
@@ -4,7 +4,7 @@ public interface IMyTask<TResult>
 {
     public bool IsCompleted { get; }
 
-    public TResult Result { get; }
+    public TResult? Result { get; }
 
-    public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuation);
+    public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult?, TNewResult> continuation);
 }
--- MyThreadPool/MyThreadPool/MyThreadPool.cs
+++ MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -1,58 +1,145 @@
 ﻿using System.Collections.Concurrent;
+using System.ComponentModel;
 using System.Threading;
 
 namespace MyThreadPool;
 
 public class MyThreadPool
 {
-    private ConcurrentQueue<Action> tasksQueue = new();
-    private AutoResetEvent queueIsNotEmptyEvent = new(false);
-    private ManualResetEvent shutDownEvent = new(false);
-    private WaitHandle[] events;
-    private Thread[] threads;
-    private CancellationTokenSource source = new();
-    private volatile int workingThreadsNumber = 0;
+    private readonly ConcurrentQueue<Action> _tasksQueue = new();
+    private readonly AutoResetEvent _queueIsNotEmptyEvent = new(false);
+    private readonly ManualResetEvent _shutDownEvent = new(false);
+    private readonly  WaitHandle[] _events;
+    private readonly Thread[] _threads;
+    private readonly CancellationTokenSource _source = new();
+    private volatile int _workingThreadsNumber = 0;
 
     public MyThreadPool(int threadsNumber)
     {
-        events = new WaitHandle[]
+        _events = new WaitHandle[]
         {
-            queueIsNotEmptyEvent,
-            shutDownEvent
+            _queueIsNotEmptyEvent,
+            _shutDownEvent
         };
-        threads = new Thread[threadsNumber];
-        for (int i = 0; i < threads.Length; i++)
+        _threads = new Thread[threadsNumber];
+        for (int i = 0; i < _threads.Length; i++)
         {
-            threads[i] = new Thread(() =>
+            _threads[i] = new Thread(() =>
             {
-                while (!source.IsCancellationRequested)
+                WaitHandle.WaitAny(_events);
+                while (!_source.IsCancellationRequested)
                 {
-                    int eventIndex = WaitHandle.WaitAny(events);
-                    if (eventIndex == 1)
-                        break;
-                    queueIsNotEmptyEvent.Reset()
-                    if (!tasksQueue.TryDequeue(out Action? task))
-                        continue;
-                    else if (task != null)
+                    if (_tasksQueue.TryDequeue(out Action? task) && task != null)
                     {
-                        workingThreadsNumber++;
+                        _workingThreadsNumber++;
                         task();
-                        workingThreadsNumber--;
+                        _workingThreadsNumber--;
                     }
+                    lock (_tasksQueue)
+                    {
+                        if (_tasksQueue.IsEmpty)
+                            _queueIsNotEmptyEvent.Reset();
+                    }
+                    WaitHandle.WaitAny(_events);
                 }
-                while (tasksQueue.Count > 0)
+                while (!_tasksQueue.IsEmpty)
                 {
-                    if (!tasksQueue.TryDequeue(out Action? task))
+                    if (!_tasksQueue.TryDequeue(out Action? task))
                         break;
                     else if (task != null)
                     {
-                        workingThreadsNumber++;
+                        _workingThreadsNumber++;
                         task();
-                        workingThreadsNumber--;
+                        _workingThreadsNumber--;
                     }
                 }
             });
-            threads[i].Start();
+            _threads[i].Start();
+        }
+    }
+
+    public void ShutDown()
+    {
+        lock (_source)
+            _source.Cancel();
+        _shutDownEvent.Set();
+        for (int i = 0; i < _threads.Length; i++)
+            _threads[i].Join();
+    }
+
+    public IMyTask<TResult> Submit<TResult>(Func<TResult> supplier)
+    {
+        lock (_source)
+        {
+            if (_source.IsCancellationRequested)
+                throw new InvalidOperationException("thread was shut down");
+            var task = new MyTask<TResult>(this, supplier);
+            _tasksQueue.Enqueue(() => task.Run());
+            return task;
+        }
+    }
+
+    private class MyTask<TResult> : IMyTask<TResult>
+    {
+        private TResult? _result;
+        private readonly Func<TResult> _supplier;
+        private Exception? _exception;
+        Action? _nextAction;
+        private readonly MyThreadPool _threadPool;
+        private readonly ManualResetEvent _resultEvent = new(false);
+        public bool IsCompleted { get; private set; }
+
+        public TResult? Result
+        {
+            get
+            {
+                _resultEvent.WaitOne();
+                if (_exception != null)
+                    throw new AggregateException(_exception);
+                return _result;
+            }
+        }
+
+        public MyTask(MyThreadPool threadPool, Func<TResult> supplier)
+        {
+            _supplier = supplier;
+            _threadPool = threadPool;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                _result = _supplier();
+            }
+
+            catch (Exception e)
+            {
+                _exception = e;
+            }
+            finally
+            {
+                IsCompleted = true;
+                if (_nextAction != null)
+                {
+                    _threadPool._tasksQueue.Enqueue(_nextAction);
+                }
+                _resultEvent.Set();
+            }
+        }
+
+        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult?, TNewResult> continuation)
+        {
+            lock (_threadPool._source)
+            {
+                if (_threadPool._source.IsCancellationRequested)
+                {
+                    throw new InvalidOperationException("thread was shut down");
+                }
+                var task = new MyTask<TNewResult>(_threadPool, () => continuation(Result));
+                _nextAction = () => task.Run();
+                return task;
+            }
         }
     }
 }
