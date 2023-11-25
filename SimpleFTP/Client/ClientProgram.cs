6ae37ca5729c8046cd09a175367f47f39b608d8c
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-11-26 01:46:40 +0300

    first version of Server class

---
+++ SimpleFTP/Client/ClientProgram.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+
+const int port = 8888;
+using (var client = new TcpClient("localhost", port))
+{
+    Writer(client.GetStream());
+    Reader(client.GetStream());
+    while (true) { }
+}
+
+static void Writer(NetworkStream stream)
+{
+    Task.Run(async () =>
+    {
+        var writer = new StreamWriter(stream) { AutoFlush = true };
+        while (true)
+        {
+            var data = Console.ReadLine();
+            Console.WriteLine("you: " + data);
+            writer.Write(data + "\n");
+        }
+    });
+}
+
+static void Reader(NetworkStream stream)
+{
+    Task.Run(() =>
+    {
+        var reader = new StreamReader(stream);
+        while (true)
+        {
+            var line = reader.ReadLine();
+            Console.WriteLine("Server: " + line);
+        }
+    });
+}
---
+++ SimpleFTP/Server/Server.cs
@@ -0,0 +1,84 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+public class Server
+{
+    private static readonly string IP = "localhost";
+    private static readonly int PORT = 8888;
+    private readonly TcpListener listener = new(IPAddress.Any, PORT);
+
+    public async Task Start()
+    {
+        listener.Start();
+        while (true)
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            ClientHander(client.GetStream());
+        }
+    }
+
+    private void ClientHander(NetworkStream stream)
+    {
+        Task.Run(async () =>
+        {
+            using var reader = new StreamReader(stream);
+            using var writer = new StreamWriter(stream);
+            var line = await reader.ReadLineAsync();
+            var splittedLine = line?.Split(" ");
+            if (splittedLine is null || splittedLine.Length != 2)
+            {
+                await writer.WriteLineAsync("incorrect request");
+                return;
+            }
+            var (request, path) = (splittedLine[0], splittedLine[1]);
+            switch (request)
+            {
+                case "1":
+                    ListRequestHandlerAsync(path, writer);
+                    break;
+                case "2":
+                    GetRequestHandlerAsync(path, writer);
+                    break;
+                default:
+                    await writer.WriteLineAsync("unknown request");
+                    break;
+            }
+        });
+    }
+
+    private async void ListRequestHandlerAsync(string path, StreamWriter writer)
+    {
+        if (!Directory.Exists(path))
+        {
+            await writer.WriteLineAsync("-1");
+            return;
+        }
+        var dirs = Directory.GetDirectories(path);
+        var files = Directory.GetFiles(path);
+        StringBuilder stringBuilder = new();
+        stringBuilder.Append($"{dirs.Length + files.Length} ");
+        foreach (var dir in dirs)
+        {
+            stringBuilder.Append($"{dir} true ");
+        }
+        foreach (var file in files)
+        {
+            stringBuilder.Append($"{file} false ");
+        }
+        await writer.WriteLineAsync(stringBuilder.ToString());
+    }
+
+    private async void GetRequestHandlerAsync(string path, StreamWriter writer)
+    {
+        if (!File.Exists(path))
+        {
+            await writer.WriteLineAsync("-1");
+            return;
+        }
+        var bytes = File.ReadAllBytes(path);
+        await writer.WriteLineAsync($"{bytes.Length} {bytes}");
+    }
+}
