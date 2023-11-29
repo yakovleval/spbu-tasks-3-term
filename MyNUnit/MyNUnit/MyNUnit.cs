1786a5f0ae2d9818ad6c4c06de82aa33fe2f6798
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-11-29 05:37:59 +0300

    implement printing results

--- MyNUnit/MyNUnit/MyNUnit.cs
+++ MyNUnit/MyNUnit/MyNUnit.cs
@@ -5,13 +5,35 @@ namespace MyNUnit;
 
 public class MyNUnit
 {
-    private List<TestAssembly>? _testAssemblies;
+    private List<TestAssembly> _testAssemblies;
     public MyNUnit(string path)
     {
-        _testAssemblies = Directory
+        var directory = Directory
             .EnumerateFiles(path, "*.dll")
+            .ToList();
+        directory.Sort();
+        _testAssemblies = directory
             .Select(Assembly.Load)
             .Select(a => new TestAssembly(a))
             .ToList();
+    }
+
+    public void RunTests()
+    {
+        List<List<Report>> result = new();
+        Parallel.Invoke(_testAssemblies
+            .Select((testClass, index) => new Action(() => result.Add(testClass.RunTests())))
+            .ToArray());
+    }
+
+    public void PrintResult(List<List<Report>> result)
+    {
+        foreach (var assemblyResult in result)
+        {
+            foreach (var report in assemblyResult)
+            {
+                Console.WriteLine(report);
+            }
+        }
     }
 }
--- MyNUnit/MyNUnit/TestAssembly.cs
+++ MyNUnit/MyNUnit/TestAssembly.cs
@@ -6,9 +6,9 @@ namespace MyNUnit;
 public class TestAssembly
 {
     private TestClass[] _testClasses;
-
     public TestAssembly(Assembly assembly)
     {
+        _assembly = assembly;
         _testClasses = assembly
             .ExportedTypes
             .Select(type => new TestClass(type))
